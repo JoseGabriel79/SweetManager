08deec3c680c72578923fc06a80eda74dd4de98a
José Gabriel <josegabriel2001biel@gmail.com>
2025-09-07 13:22:12 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,28 @@
+// const { Pool } = require("pg");
+// require("dotenv").config();
+
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   ssl: {
+//     rejectUnauthorized: false, // Railway exige SSL em alguns casos
+//   },
+// });
+
+// module.exports = pool;
+
 const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
   user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
   password: process.env.DB_PASS,
-  ssl: {
-    rejectUnauthorized: false, // Railway exige SSL em alguns casos
-  },
+  port: 5432,
 });
 
 module.exports = pool;
