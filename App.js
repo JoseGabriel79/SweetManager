2d5e11fe73079c9f940897d4f5014aee84676eaf
José Gabriel <josegabriel2001biel@gmail.com>
2025-09-07 13:16:05 -0300

    Update App.js

--- App.js
+++ App.js
@@ -96,14 +96,25 @@ import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
+
   const [status, setStatus] = useState("Carregando...");
 
   useEffect(() => {
-    fetch("https://duzeapp-production.up.railway.app/ping") // troque pelo IP da sua máquina
-      .then((res) => res.json())
-      .then((data) => setStatus("Conectado: " + JSON.stringify(data)))
-      .catch((err) => setStatus("Erro: " + err.message));
-  }, []);
+  fetch("https://duzeapp-production.up.railway.app/ping")
+    .then(async (res) => {
+      const text = await res.text(); // pega o que veio de verdade
+      console.log("Resposta bruta:", text);
+
+      try {
+        const data = JSON.parse(text);
+        setStatus("Conectado: " + JSON.stringify(data));
+      } catch {
+        setStatus("Não é JSON: " + text);
+      }
+    })
+    .catch((err) => setStatus("Erro: " + err.message));
+}, []);
+
 
   return (
     <View style={styles.container}>
