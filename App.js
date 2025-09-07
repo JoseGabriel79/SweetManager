f1e53d4354b49e24c4e0819eeb219049a5c21fe9
José Gabriel <josegabriel2001biel@gmail.com>
2025-09-07 13:26:58 -0300

    Update App.js

--- App.js
+++ App.js
@@ -100,7 +100,7 @@ export default function App() {
   const [status, setStatus] = useState("Carregando...");
 
   useEffect(() => {
-    fetch("https://meu-backend.up.railway.app/produtos") // substitua pela URL do Railway
+    fetch("https://duzeapp-production.up.railway.app/produtos") // substitua pela URL do Railway
       .then(async (res) => {
         const text = await res.text();
         console.log("Resposta bruta:", text);
