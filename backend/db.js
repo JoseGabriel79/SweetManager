b83f3287fa70af002f4ad352a4e2f9e9c8b9a7de
José Gabriel <josegabriel2001biel@gmail.com>
2025-09-07 14:44:02 -0300

    .

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const pool = new Pool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 3000,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
