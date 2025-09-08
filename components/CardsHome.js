bc94b057583ea0f69bcf9f2600084394df94c6c9
José Gabriel <josegabriel2001biel@gmail.com>
2025-09-08 20:10:34 -0300

    Update CardsHome.js

--- components/CardsHome.js
+++ components/CardsHome.js
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
         height:isSmallScreen?"20%":"25%",
     },
     title: {
-        fontSize: 18,
+        fontSize: 20,
         fontWeight: "bold",
         color: "white",
         textAlign: "center",
