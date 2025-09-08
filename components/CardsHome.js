96e97c8fa9c7384d29cdc7f1f01496742cb37599
José Gabriel <josegabriel2001biel@gmail.com>
2025-09-08 19:49:02 -0300

    Update CardsHome.js

--- components/CardsHome.js
+++ components/CardsHome.js
@@ -6,28 +6,28 @@ import {
 } from "react-native";
 
 export default function ({ titulo }) {
-    return  <TouchableOpacity style={styles.card}>
-        <Text style={styles.title}>{titulo}</Text>
-       
+    return (
+        <TouchableOpacity style={styles.card}>
+            <Text style={styles.title}>{titulo}</Text>
+
         </TouchableOpacity>
+    )
 }
 
 const styles = StyleSheet.create({
     card: {
         backgroundColor: "#51AFF9",
         borderRadius: 12,
-        padding: 16,
         margin: 8,
         width: "45%", // 2 cards por linha
         alignItems: "center",
         justifyContent: "center",
-        height:"20%",
+        height: "25%",
     },
     title: {
         fontSize: 18,
         fontWeight: "bold",
         color: "white",
-        marginBottom: 12,
         textAlign: "center",
     },
 });
