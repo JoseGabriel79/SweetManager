ddc01587e0158b66b620700930667a27745bec43
José Gabriel <josegabriel2001biel@gmail.com>
2025-09-12 13:51:59 -0300

    Update HomeScreen.js

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -9,7 +9,7 @@ export default function HomeScreen({ usuario }) {
 
   useEffect(() => {
     if (usuario) {
-      setDadosUsuario({usuario});
+      setDadosUsuario({ ...usuario });
       console.log("usuario antes de passar os dados"+ usuario)
       console.log("usuario depois de passar os dados"+ dadosUsuario)
     } else {
